f2083ec5f3ad726eedd9a56d733829f1186ae46f
Eryc Tri Juni S <147351364+ErycTheGreat@users.noreply.github.com>
2026-04-18 03:07:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -515,7 +515,7 @@ const wakeUpScript = `
                 } else {
                     triggerBg(); 
                 }
-            }, 400); 
+            }, 200); 
         });
     })();
 </script>`;
