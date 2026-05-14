831e3d2dab429eed65ed3cf399938b6bfa5afa13
Eryc Tri Juni S <147351364+ErycTheGreat@users.noreply.github.com>
2026-05-14 17:19:25 +0700

    Add meta-externalfetcher and meta-webindexer to bot detection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export default {
 
     // --- 0.1 BOT TRACKER & DETECTION ---
 	const userAgent = request.headers.get("User-Agent") || "";
-	const isAIBot = /OAI-SearchBot|ChatGPT-User|GPTBot|PerplexityBot|Perplexity-User|Google-Extended|ClaudeBot|Claude-User|Claude-SearchBot|claude-web|AmazonBot|Cohere-AI|YouBot|Bytespider|Applebot-Extended/i.test(userAgent);
+	const isAIBot = /OAI-SearchBot|ChatGPT-User|GPTBot|PerplexityBot|Perplexity-User|Google-Extended|ClaudeBot|Claude-User|Claude-SearchBot|claude-web|AmazonBot|Cohere-AI|YouBot|Bytespider|Applebot-Extended|meta-externalfetcher|meta-webindexer/i.test(userAgent);
 	const isSEOBot = /googlebot|bingbot|yandexbot|slurp|duckduckbot|ahrefsbot|semrushbot|SiteAuditBot|MBCrawler|MJ12bot|DotBot|seositecheckup|Applebot/i.test(userAgent);
 	const isSocialBot = /facebookexternalhit|Facebot|FacebookBot|twitterbot|whatsapp|linkedinbot|pinterest|telegrambot|discordbot|Slackbot|meta-external.?agent/i.test(userAgent);
 	const isCrawlerBot = /CCBot|ia_archiver|archive\.org_bot|Scrapy/i.test(userAgent);
