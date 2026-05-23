dfd8858b92b66cf900a0d76ae07a85ce05647523
Eryc Tri Juni S <147351364+ErycTheGreat@users.noreply.github.com>
2026-05-23 21:24:31 +0700

    Update bot condition for signal pruning

--- index.js
+++ index.js
@@ -842,7 +842,7 @@ const wakeUpScript = `
     }
 
     // 🔪 SIGNAL PRUNING: Kill CMS garbage for AI models
-    if (isBot) {
+    if (isAIBot || isSocialBot) {
         rewriter
             .on('script', new ElementSlasher())    
             .on('style', new ElementSlasher())        
